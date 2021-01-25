dfda902571a81a9dc954442f69ffe0818e1f68de
BotZeus <77181929+BotZeus@users.noreply.github.com>
2021-01-24 22:49:55 -0300

    Add files via upload

---
+++ scr/botinhomenu.js
@@ -0,0 +1,12 @@
+const botinhomenu = (prefix, pushname) => {
+    return `◪ *Comandos do BotinhoPremium-v6*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.botinhomenu = botinhomenu
