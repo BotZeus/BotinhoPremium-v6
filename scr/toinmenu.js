40aa33bfc558e298f33ca7f14eaa30e0a374e3a9
BotZeus <77181929+BotZeus@users.noreply.github.com>
2021-01-25 18:29:34 -0300

    Add files via upload

---
+++ scr/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do BotinhoPremium-v6*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinomenu
